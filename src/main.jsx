8b3caa9682ef7341daa8bc79faa7f65ae54fd837
MogaMAW <moga.m.abdulwahab@gmail.com>
2023-04-01 21:37:53 +0300

    Email service under implemention

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { ProfileProvider } from "./context/ProfileContext";
+import { ContextProvider } from "./context/ContextProvider";
 
 const theme = createTheme({
   typography: {
@@ -19,9 +19,9 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <ProfileProvider>
+      <ContextProvider>
         <App />
-      </ProfileProvider>
+      </ContextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
